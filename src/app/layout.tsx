5e78f0f00f9cecdecbc904ba9a38d0e86911d85b
Rowan <1962408497@qq.com>
2024-08-13 19:03:23 +0800

    feat: 引入 Next-ui 组件库

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
+import "../assets/styles/globals.css";
+import React from "react";
+import ClientProvider from "@/components/ClientProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -10,13 +12,17 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body className={inter.className}>
+    <ClientProvider>
+      {children}
+    </ClientProvider>
+    </body>
     </html>
   );
 }
